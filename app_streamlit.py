464ab3af68d2c1105c6019b7ef9710e50b9fa1ad
Aniket Saxena <92912434+fork123aniket@users.noreply.github.com>
2023-08-17 17:54:52 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -141,6 +141,11 @@ def main():
                                     hover_data=["score", "feature_id"],
                                     height=400,
                                     title=f"Feature importance for all {len(data)} features")
+                bar_figure.update_layout(
+                    hoverlabel=dict(
+                        bgcolor="black"
+                    )
+                )
                 tab1, tab2, tab3, tab4 = st.tabs([
                     'Predicted Class', 'Original Subgraph',
                     'Feature Importance', 'Explained Subgraph'
